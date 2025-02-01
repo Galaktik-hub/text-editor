24391c9e97e0a62bc0cd6a4361d6ace815ba05f5
Galaktik-hub <dvatheking@gmail.com>
2025-02-01 23:36:03 +0100

    Buffer implementation + cursorPosition more robust

--- editor.c
+++ editor.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -22,6 +23,13 @@ struct editorConfig {
 };
 
 struct editorConfig E;
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
 
 /*** terminal ***/
 
@@ -78,11 +86,42 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+
+    // The start end of a string should be \0
+    buf[i] = '\0';
+
+    // We check if the response start with an escape sequence
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    // We parse the answer
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+
+    return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+        // We move the cursor with the C (cursor forward) and B (cursor down) commands
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -90,29 +129,54 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** append buffer ***/
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL)
+        return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
 /*** output **/
 
 // To draw the left column of characters
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
-    for (y = 0; y < 24; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+    for (y = 0; y < E.screenrows; y++) {
+        abAppend(ab, "~", 1);
+
+        if (y < E.screenrows - 1)
+            abAppend(ab, "\r\n", 2);
     }
 }
 
 // To initiliaze the display
 void editorRefreshScreen() {
+    struct abuf ab = ABUF_INIT;
+
     // \x1b is the byte that represent the escape character
     // J is meant to clear the screen
     // 2 is meant to specify we want to clear the entire screen
-    write(STDOUT_FILENO, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[2J", 4);
     // H is meant to position the cursor on the terminal
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
 
-    editorDrawRows();
+    editorDrawRows(&ab);
 
     // After we finished drawing the left column, we reposition the cursor
     write(STDOUT_FILENO, "\x1b[H", 3);
+
+    // We dump the buffer
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** input ***/
@@ -135,7 +199,7 @@ void editorProcessKeypress() {
 
 void initEditor() {
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
-        die('getWindowSize failed');
+        die("getWndowSize failed");
 }
 
 int main() {
@@ -146,6 +210,6 @@ int main() {
         editorRefreshScreen();
         editorProcessKeypress();
     }
-    
+
     return 0;
 }
