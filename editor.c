3cc7e57a23de784a8e79dc626c829a7bc7cc62fd
Galaktik-hub <dvatheking@gmail.com>
2025-02-03 21:34:31 +0100

    Key press interpreted

--- editor.c
+++ editor.c
@@ -11,12 +11,17 @@
 
 /*** defines ***/
 
+#define CONFEDIT_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+#define ABUF_INIT {NULL, 0}
 
 /*** data ***/
 
 // A global structure to keep track of the editor state
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -29,7 +34,17 @@ struct abuf {
     int len;
 };
 
-#define ABUF_INIT {NULL, 0}
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
 
 /*** terminal ***/
 
@@ -74,7 +89,7 @@ void enableRawMode() {
 }
 
 // Function that returns input keypress
-char editorReadKey() {
+int editorReadKey() {
     int nread;
     char c;
 
@@ -83,7 +98,48 @@ char editorReadKey() {
             die("read failed");
     }
 
-    return c;
+    if (c == '\x1b') {
+        char seq[3];
+        
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1':
+                        case '7':
+                            return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4':
+                        case '8':
+                            return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                } 
+            } else {
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
+            switch (seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+            }
+        }
+        return '\x1b';
+    } else {
+        return c;
+    }
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -147,12 +203,29 @@ void abFree(struct abuf *ab) {
 
 /*** output **/
 
-// To draw the left column of characters
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome),
+                "EditConfig -- version %s", CONFEDIT_VERSION);
 
+            if (welcomelen > E.screencols) welcomelen = E.screencols;
+
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while(padding--) abAppend(ab, " ", 1);
+
+            abAppend(ab, welcome, welcomelen);
+        } else {
+            abAppend(ab, "~", 1);
+        }
+
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1)
             abAppend(ab, "\r\n", 2);
     }
@@ -162,17 +235,23 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
+    // We hide the cursor
+    abAppend(&ab, "\x1b[?25l", 6);
+
     // \x1b is the byte that represent the escape character
-    // J is meant to clear the screen
-    // 2 is meant to specify we want to clear the entire screen
-    abAppend(&ab, "\x1b[2J", 4);
-    // H is meant to position the cursor on the terminal
+    // H is meant to position the cursor on the terminal, by default, at 1:1
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     // After we finished drawing the left column, we reposition the cursor
     write(STDOUT_FILENO, "\x1b[H", 3);
+    // We display back the cursor
+    abAppend(&ab, "\x1b[?25h", 6);
 
     // We dump the buffer
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -181,9 +260,30 @@ void editorRefreshScreen() {
 
 /*** input ***/
 
+void editorMoveCursor(int key) {
+    switch (key) {
+        case ARROW_LEFT:
+            if (E.cx != 0)
+                E.cx--;
+            break;
+        case ARROW_RIGHT:
+            if (E.cx != E.screencols - 1)
+                E.cx++;
+            break;
+        case ARROW_UP:
+            if (E.cy != 0) 
+                E.cy--;
+            break;
+        case ARROW_DOWN:
+            if (E.cy != E.screenrows - 1)
+                E.cy++;
+            break;
+    }
+}
+
 // Waits for a key to be pressed then handle it
 void editorProcessKeypress() {
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
@@ -192,12 +292,39 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while (times--)
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+
+        case ARROW_UP:
+        case ARROW_DOWN:
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
+            break;
     }
 }
 
 /*** init **/
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
         die("getWndowSize failed");
 }
