f2e42679b6b511cf3fcff99ef3f6875cc2852757
Galaktik-hub <dvatheking@gmail.com>
2025-02-04 09:04:19 +0100

    Cursor improvements

--- editor.c
+++ editor.c
@@ -341,12 +341,18 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if (E.cx != 0)
                 E.cx--;
+            else if (E.cx < E.numrows) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size)
                 E.cx++;
-            else
+            else if (E.cy != 0) {
                 E.cx = 0;
+                E.cy++;
+            }
             break;
         case ARROW_UP:
             if (E.cy != 0) 
@@ -355,6 +361,8 @@ void editorMoveCursor(int key) {
         case ARROW_DOWN:
             if (E.cy < E.numrows)
                 E.cy++;
+            if (E.cx > E.row[E.cy].size)
+                E.cx = E.row[E.cy].size;
             break;
     }
 }
